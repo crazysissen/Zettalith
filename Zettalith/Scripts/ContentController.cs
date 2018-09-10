f1dfdaaad838e467c589c0caf7c57a459a3d32e8
Sixten Schelin <crazysissen@gmail.com>
2018-09-10 22:01:57 +0200

    Opposite and incorrect

---
+++ Zettalith/Scripts/ContentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zettalith
+{
+    class ContentController
+    {
+        static ContentController singleton;
+
+        Dictionary<string, object> contentDictionary;
+
+        public ContentController()
+        {
+            singleton = this;
+            contentDictionary = new Dictionary<string, object>();
+        }
+
+        public static T Get<T>(string tag)
+        {
+            try
+            {
+                if (singleton.contentDictionary[tag] is T)
+                {
+                    return (T)singleton.contentDictionary[tag];
+                }
+
+                return default(T);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+    }
+}
