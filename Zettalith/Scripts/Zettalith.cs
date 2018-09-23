0058bb5873d705e00f9a549e72ecddd7b5dc490b
Benjamin Ek <benjamin.ek1@learnet.se>
2018-09-23 14:28:10 +0200

    Superior kod //Benjamin

---
+++ Zettalith/Scripts/Zettalith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zettalith.Pieces
+{
+    class Zettalith
+    {
+        public Mana ManaCost => top.ManaCost + middle.ManaCost + bottom.ManaCost;
+
+        Top top;
+        Middle middle;
+        Bottom bottom;
+
+        public Zettalith(Top top, Middle middle, Bottom bottom)
+        {
+            this.top = top;
+            this.middle = middle;
+            this.bottom = bottom;
+        }
+    }
+}
