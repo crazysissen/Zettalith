fc5c9b93d0995446ec6220989735bd1823554fd0
Benjamin Ek <benjamin.ek1@learnet.se>
2018-10-01 09:38:23 +0200

    some of dat data

--- Zettalith/Scripts/Pieces/SubPiece.cs
+++ Zettalith/Scripts/Pieces/SubPiece.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Zettalith.Pieces
 {
     abstract class SubPiece
     {
         public abstract Mana ManaCost { get; set; }
+        public abstract int SubHealth { get; set; }
+        public abstract int AttackDamage { get; set; }
 
     }
 }
