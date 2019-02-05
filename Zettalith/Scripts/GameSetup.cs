b59eb1afb90eb7f1b9105ea51a21cfe5d4db2b2c
Sixten Schelin <crazysissen@gmail.com>
2019-02-05 13:57:55 +0100

    yretug

---
+++ Zettalith/Scripts/GameSetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zettalith
+{
+    class GameSetup
+    {
+        private MainController controller;
+        private InGameController inGameController;
+
+        public void Initialize(MainController controller, InGameController inGameController)
+        {
+            this.controller = controller;
+            this.inGameController = inGameController;
+        }
+
+        public void Update(float deltaTime)
+        {
+
+        }
+    }
+}
