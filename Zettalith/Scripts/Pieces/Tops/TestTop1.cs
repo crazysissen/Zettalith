67433dc45aa4273c47986e55e3bac994c6ae94b9
MansLBS <MansLBS@users.noreply.github.com>
2019-02-06 14:56:49 +0100

    Håller på med saker

--- Zettalith/Scripts/Pieces/Tops/TestTop1.cs
+++ Zettalith/Scripts/Pieces/Tops/TestTop1.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Zettalith.Pieces
 {
     class TestTop1 : Top
     {
-
+        TestTop1()
+        {
+            Name = "TestTop1";
+            Health = 10;
+            AttackDamage = 7;
+            ManaCost = new Mana(5, 0, 0);
+            Description = "Frail but powerful head.";
+            Texture = Load.Get<Texture2D>("TestSubpiece");
+        }
     }
 }
