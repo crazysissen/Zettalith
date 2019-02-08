712ab45d778cf63d164d88e90f551a0a0c31d694
MansLBS <MansLBS@users.noreply.github.com>
2019-02-08 17:18:59 +0100

    Settings coming along. Robustified main menu

--- Zettalith/Scripts/MainController.cs
+++ Zettalith/Scripts/MainController.cs
@@ -37,6 +37,7 @@ namespace Zettalith
         private InGameController inGameController;
         private MainMenu mainMenu;
         private SetDesigner setDesigner;
+        private SettingsMenu settingsMenu;
         private Lobby lobby;
 
         public MainController()
@@ -169,6 +170,14 @@ namespace Zettalith
 
             setDesigner = new SetDesigner();
             setDesigner.Initialize(this);
+        }
+
+        public void ToSettings(Action goBackTO)
+        {
+            mainMenu.CloseMenu();
+
+            settingsMenu = new SettingsMenu();
+            settingsMenu.Initialize(this, goBackTO);
         }
 
         public void ToLobby(StartupConfig? config = null)
