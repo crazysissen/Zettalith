d99d78528f135721f008495003b6b4b3e081b676
Sixten Schelin <crazysissen@gmail.com>
2019-02-11 14:37:39 +0100

    drttuygu

--- Zettalith/Scripts/Pieces/Subpieces.cs
+++ Zettalith/Scripts/Pieces/Subpieces.cs
@@ -14,7 +14,7 @@ namespace Zettalith
         {
             typeof(TestTop1), typeof(TestTop2), //typeof(Top2),
             typeof(TestMiddle1), typeof(TestMiddle2), //typeof(Middle2),
-            typeof(TestBottom1), typeof(TestBottom2), typeof(Straight), typeof(Diagonal), //typeof(Bottom2)
+            typeof(TestBottom1), typeof(TestBottom2), typeof(Straight), typeof(Diagonal), typeof(TestTop3) //typeof(Bottom2)
         };
 
         // Bool values here decides if a subpiece is unlocked or not
@@ -23,7 +23,7 @@ namespace Zettalith
         {
             true, true, /*Tops*/
             true, true, /*Middles*/
-            true, true, true, true, /*Bottoms*/
+            true, true, true, true, true /*Bottoms*/
         };
 
         // Creates a SubPiece from a selected index (in Subpieces.subpieces list)
---
+++ Zettalith/Scripts/Pieces/Tops/TestTop3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Zettalith.Pieces
+{
+    class TestTop3 : Top
+    {
+        public TestTop3()
+        {
+            Name = "Woop";
+            ManaCost = new Mana(4, 2, 0);
+            Texture = Load.Get<Texture2D>("TestSubpiece");
+        }
+    }
+}
