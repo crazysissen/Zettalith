3ccc6f9fa446daf7b8be5a5a2d268dc2276fb7fd
Sixten Schelin <crazysissen@gmail.com>
2019-02-11 14:10:46 +0100

    comit

--- Zettalith/Scripts/In Game/TileObject.cs
+++ Zettalith/Scripts/In Game/TileObject.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Zettalith
 {
     abstract class TileObject
     {
+        public int Index { get; set; }
+        public Point Position { get; set; }
+
         public TileObject()
         {
             
