84f736bca397b0747f18149160ad03ed2aec4689
Sixten Schelin <sixten.schelin@learnet.se>
2019-04-15 15:55:30 +0200

    asd

---
+++ Zettalith/Scripts/In Game/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Zettalith
+{
+    class CameraMovement
+    {
+        Vector2 previousWorldPosition;
+
+        public void Update(Camera camera, Point mousePosition, bool mouseOnEmpty)
+        {
+            Vector2 initialWorldPosition = camera.ScreenToWorldPosition(mousePosition.ToVector2());
+
+            if (In.MouseState.MiddleButton == ButtonState.Pressed)
+            {
+                camera.Position += new Vector2(previousWorldPosition.X - initialWorldPosition.X, previousWorldPosition.Y - initialWorldPosition.Y);
+            }
+
+            previousWorldPosition = camera.ScreenToWorldPosition(mousePosition.ToVector2());
+        }
+    }
+}
