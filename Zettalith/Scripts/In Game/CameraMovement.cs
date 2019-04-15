80ea02dfc072c8a915f81c864dbe6f95cfcfb4b7
Måns Berggren <mans.berggren1@learnet.se>
2019-04-15 20:09:54 +0200

    CAMERA STUFFS

--- Zettalith/Scripts/In Game/CameraMovement.cs
+++ Zettalith/Scripts/In Game/CameraMovement.cs
@@ -12,6 +12,8 @@ namespace Zettalith
     {
         Vector2 previousWorldPosition;
         Vector2 cameraSpeed = new Vector2();
+        const float cameraScaleZoom = 0.15f, cameraKeyboardMove = 0.5f;
+        int previousScrollWheelValue;
 
         public void Update(Camera camera, Point mousePosition, bool mouseOnEmpty, float deltaTime)
         {
@@ -25,12 +27,48 @@ namespace Zettalith
             }
             else
             {
-                cameraSpeed *= 0.9f * (1f - deltaTime);
+                cameraSpeed *= 0.8f * (1f - deltaTime);
                 camera.Position += cameraSpeed * 2;
             }
 
+            if (In.MouseState.ScrollWheelValue != previousScrollWheelValue)
+            {
+                float tempCameraScaleModifier = 1;
+                Vector2 tempCameraPositionAddend = new Vector2();
+
+                // Zoomat in
+                if (0 < In.MouseState.ScrollWheelValue - previousScrollWheelValue)
+                {
+                    tempCameraScaleModifier = 1f + cameraScaleZoom;
+                    tempCameraPositionAddend = 0.13f * (camera.ScreenToWorldPosition(mousePosition.ToVector2()) - camera.ScreenToWorldPosition(camera.CenterCoordinate));
+                }
+
+                // Zoomat ut
+                if (0 > In.MouseState.ScrollWheelValue - previousScrollWheelValue)
+                {
+                    tempCameraScaleModifier = 1f - cameraScaleZoom;
+                    tempCameraPositionAddend = -1 * 0.175f * (camera.ScreenToWorldPosition(mousePosition.ToVector2()) - camera.ScreenToWorldPosition(camera.CenterCoordinate));
+                }
+
+                for (int i = 0; i < Math.Abs((In.MouseState.ScrollWheelValue - previousScrollWheelValue) / 120); ++i)
+                {
+                    camera.Scale *= tempCameraScaleModifier;
+                    camera.Position += tempCameraPositionAddend;
+                }
+            }
+
+            if (In.Key(Keys.W))
+                camera.Position = new Vector2(camera.Position.X, camera.Position.Y - cameraKeyboardMove);
+            if (In.Key(Keys.A))
+                camera.Position = new Vector2(camera.Position.X - cameraKeyboardMove, camera.Position.Y);
+            if (In.Key(Keys.S))
+                camera.Position = new Vector2(camera.Position.X, camera.Position.Y + cameraKeyboardMove);
+            if (In.Key(Keys.D))
+                camera.Position = new Vector2(camera.Position.X + cameraKeyboardMove, camera.Position.Y);
+
 
             previousWorldPosition = camera.ScreenToWorldPosition(mousePosition.ToVector2());
+            previousScrollWheelValue = In.MouseState.ScrollWheelValue;
         }
     }
 }
