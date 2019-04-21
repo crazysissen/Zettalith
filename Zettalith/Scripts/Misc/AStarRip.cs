4f0a83bcbb386718ff7230d9d36aa2eee33ab7d9
Sixten Schelin <sixten.schelin@learnet.se>
2019-04-21 20:39:06 +0200

    there we goo

--- Zettalith/Scripts/Misc/AStarRip.cs
+++ Zettalith/Scripts/Misc/AStarRip.cs
@@ -1,290 +1,293 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Collections;
-//using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.Xna.Framework;
 
-//public class Astar
-//{
-//        public List<Vector2> result = new List<Vector2>();
-//        private string find;
+namespace Zettalith
+{
+    public class Astar
+    {
+        public List<Vector2> result = new List<Vector2>();
+        private string find;
 
-//        private class ASObject
-//        {
-//            public int x
-//            {
-//                get;
-//                set;
-//            }
-//            public int y
-//            {
-//                get;
-//                set;
-//            }
-//            public double f
-//            {
-//                get;
-//                set;
-//            }
-//            public double g
-//            {
-//                get;
-//                set;
-//            }
-//            public int v
-//            {
-//                get;
-//                set;
-//            }
-//            public ASObject p
-//            {
-//                get;
-//                set;
-//            }
-//            public ASObject(int x, int y)
-//            {
-//                this.x = x;
-//                this.y = y;
-//            }
-//        }
+        private class ASObject
+        {
+            public int X
+            {
+                get;
+                set;
+            }
 
-//        private ASObject[] diagonalSuccessors(bool xN, bool xS, bool xE, bool xW, int N, int S, int E, int W, int[][] grid, int rows, int cols, ASObject[] result, int i)
-//        {
-//            if (xN)
-//            {
-//                if (xE && grid[N][E] == 0)
-//                {
-//                    result[i++] = new ASObject(E, N);
-//                }
-//                if (xW && grid[N][W] == 0)
-//                {
-//                    result[i++] = new ASObject(W, N);
-//                }
-//            }
-//            if (xS)
-//            {
-//                if (xE && grid[S][E] == 0)
-//                {
-//                    result[i++] = new ASObject(E, S);
-//                }
-//                if (xW && grid[S][W] == 0)
-//                {
-//                    result[i++] = new ASObject(W, S);
-//                }
-//            }
-//            return result;
-//        }
+            public int Y
+            {
+                get;
+                set;
+            }
 
-//        private ASObject[] diagonalSuccessorsFree(bool xN, bool xS, bool xE, bool xW, int N, int S, int E, int W, int[][] grid, int rows, int cols, ASObject[] result, int i)
-//        {
-//            xN = N > -1;
-//            xS = S < rows;
-//            xE = E < cols;
-//            xW = W > -1;
+            public double F
+            {
+                get;
+                set;
+            }
 
-//            if (xE)
-//            {
-//                if (xN && grid[N][E] == 0)
-//                {
-//                    result[i++] = new ASObject(E, N);
-//                }
-//                if (xS && grid[S][E] == 0)
-//                {
-//                    result[i++] = new ASObject(E, S);
-//                }
-//            }
-//            if (xW)
-//            {
-//                if (xN && grid[N][W] == 0)
-//                {
-//                    result[i++] = new ASObject(W, N);
-//                }
-//                if (xS && grid[S][W] == 0)
-//                {
-//                    result[i++] = new ASObject(W, S);
-//                }
-//            }
-//            return result;
-//        }
+            public double G
+            {
+                get;
+                set;
+            }
 
-//        private ASObject[] nothingToDo(bool xN, bool xS, bool xE, bool xW, int N, int S, int E, int W, int[][] grid, int rows, int cols, ASObject[] result, int i)
-//        {
-//            return result;
-//        }
+            public int V
+            {
+                get;
+                set;
+            }
 
-//        private ASObject[] successors(int x, int y, int[][] grid, int rows, int cols)
-//        {
-//            int N = y - 1;
-//            int S = y + 1;
-//            int E = x + 1;
-//            int W = x - 1;
+            public ASObject P
+            {
+                get;
+                set;
+            }
 
-//            bool xN = N > -1 && grid[N][x] == 0;
-//            bool xS = S < rows && grid[S][x] == 0;
-//            bool xE = E < cols && grid[y][E] == 0;
-//            bool xW = W > -1 && grid[y][W] == 0;
+            public ASObject(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
 
-//            int i = 0;
+        private ASObject[] DiagonalSuccessors(bool xN, bool xS, bool xE, bool xW, int N, int S, int E, int W, int[][] grid, int rows, int cols, ASObject[] result, int i)
+        {
+            if (xN)
+            {
+                if (xE && grid[N][E] == 0)
+                {
+                    result[i++] = new ASObject(E, N);
+                }
+                if (xW && grid[N][W] == 0)
+                {
+                    result[i++] = new ASObject(W, N);
+                }
+            }
+            if (xS)
+            {
+                if (xE && grid[S][E] == 0)
+                {
+                    result[i++] = new ASObject(E, S);
+                }
+                if (xW && grid[S][W] == 0)
+                {
+                    result[i++] = new ASObject(W, S);
+                }
+            }
+            return result;
+        }
 
-//            ASObject[] result = new ASObject[8];
+        private ASObject[] DiagonalSuccessorsFree(bool xN, bool xS, bool xE, bool xW, int N, int S, int E, int W, int[][] grid, int rows, int cols, ASObject[] result, int i)
+        {
+            xN = N > -1;
+            xS = S < rows;
+            xE = E < cols;
+            xW = W > -1;
 
-//            if (xN)
-//            {
-//                result[i++] = new ASObject(x, N);
-//            }
-//            if (xE)
-//            {
-//                result[i++] = new ASObject(E, y);
-//            }
-//            if (xS)
-//            {
-//                result[i++] = new ASObject(x, S);
-//            }
-//            if (xW)
-//            {
-//                result[i++] = new ASObject(W, y);
-//            }
+            if (xE)
+            {
+                if (xN && grid[N][E] == 0)
+                {
+                    result[i++] = new ASObject(E, N);
+                }
+                if (xS && grid[S][E] == 0)
+                {
+                    result[i++] = new ASObject(E, S);
+                }
+            }
+            if (xW)
+            {
+                if (xN && grid[N][W] == 0)
+                {
+                    result[i++] = new ASObject(W, N);
+                }
+                if (xS && grid[S][W] == 0)
+                {
+                    result[i++] = new ASObject(W, S);
+                }
+            }
+            return result;
+        }
 
-//            ASObject[] obj =
-//                (this.find == "Diagonal" || this.find == "Euclidean") ? diagonalSuccessors(xN, xS, xE, xW, N, S, E, W, grid, rows, cols, result, i) :
-//                (this.find == "DiagonalFree" || this.find == "EuclideanFree") ? diagonalSuccessorsFree(xN, xS, xE, xW, N, S, E, W, grid, rows, cols, result, i) :
-//                                                                                         nothingToDo(xN, xS, xE, xW, N, S, E, W, grid, rows, cols, result, i);
+        private ASObject[] Successors(int x, int y, int[][] grid, int rows, int cols)
+        {
+            int N = y - 1;
+            int S = y + 1;
+            int E = x + 1;
+            int W = x - 1;
 
-//            return obj;
-//        }
+            bool xN = N > -1 && grid[N][x] == 0;
+            bool xS = S < rows && grid[S][x] == 0;
+            bool xE = E < cols && grid[y][E] == 0;
+            bool xW = W > -1 && grid[y][W] == 0;
 
-//        private double diagonal(ASObject start, ASObject end)
-//        {
-//            return Math.Max(Math.Abs(start.x - end.x), Math.Abs(start.y - end.y));
-//        }
+            int i = 0;
 
-//        private double euclidean(ASObject start, ASObject end)
-//        {
-//            var x = start.x - end.x;
-//            var y = start.y - end.y;
+            ASObject[] result = new ASObject[8];
 
-//            return Math.Sqrt(x * x + y * y);
-//        }
+            if (xN)
+            {
+                result[i++] = new ASObject(x, N);
+            }
+            if (xE)
+            {
+                result[i++] = new ASObject(E, y);
+            }
+            if (xS)
+            {
+                result[i++] = new ASObject(x, S);
+            }
+            if (xW)
+            {
+                result[i++] = new ASObject(W, y);
+            }
 
-//        private double manhattan(ASObject start, ASObject end)
-//        {
-//            return Math.Abs(start.x - end.x) + Math.Abs(start.y - end.y);
-//        }
+            ASObject[] obj =
+                (this.find == "Diagonal" || this.find == "Euclidean") ? DiagonalSuccessors(xN, xS, xE, xW, N, S, E, W, grid, rows, cols, result, i) :
+                (this.find == "DiagonalFree" || this.find == "EuclideanFree") ? DiagonalSuccessorsFree(xN, xS, xE, xW, N, S, E, W, grid, rows, cols, result, i) :
+                                                                                         result;
 
-//        public Astar(int[][] grid, int[] s, int[] e, string f)
-//        {
-//            this.find = (f == null) ? "Diagonal" : f;
+            return obj;
+        }
 
-//            int cols = grid[0].Length;
-//            int rows = grid.Length;
-//            int limit = cols * rows;
-//            int length = 1;
+        private double Diagonal(ASObject start, ASObject end)
+        {
+            return Math.Max(Math.Abs(start.X - end.X), Math.Abs(start.Y - end.Y));
+        }
 
-//            List<ASObject> open = new List<ASObject>();
-//            open.Add(new ASObject(s[0], s[1]));
-//            open[0].f = 0;
-//            open[0].g = 0;
-//            open[0].v = s[0] + s[1] * cols;
+        private double Euclidean(ASObject start, ASObject end)
+        {
+            var x = start.X - end.X;
+            var y = start.Y - end.Y;
 
-//            ASObject current;
+            return Math.Sqrt(x * x + y * y);
+        }
 
-//            List<int> list = new List<int>();
+        private double Manhattan(ASObject start, ASObject end)
+        {
+            return Math.Abs(start.X - end.X) + Math.Abs(start.Y - end.Y);
+        }
 
-//            double distanceS;
-//            double distanceE;
+        public Astar(int[][] grid, int[] s, int[] e, string f)
+        {
+            this.find = (f == null) ? "Diagonal" : f;
 
-//            int i;
-//            int j;
+            int cols = grid[0].Length;
+            int rows = grid.Length;
+            int limit = cols * rows;
+            int length = 1;
 
-//            double max;
-//            int min;
+            List<ASObject> open = new List<ASObject>();
+            open.Add(new ASObject(s[0], s[1]));
+            open[0].F = 0;
+            open[0].G = 0;
+            open[0].V = s[0] + s[1] * cols;
 
-//            ASObject[] next;
-//            ASObject adj;
+            ASObject current;
 
-//            ASObject end = new ASObject(e[0], e[1]);
-//            end.v = e[0] + e[1] * cols;
+            List<int> list = new List<int>();
 
-//            bool inList;
+            double distanceS;
+            double distanceE;
 
-//            do
-//            {
-//                max = limit;
-//                min = 0;
+            int i;
+            int j;
 
-//                for (i = 0; i < length; i++)
-//                {
-//                    if (open[i].f < max)
-//                    {
-//                        max = open[i].f;
-//                        min = i;
-//                    }
-//                }
+            double max;
+            int min;
 
-//                current = open[min];
-//                open.RemoveAt(min);
+            ASObject[] next;
+            ASObject adj;
 
-//                if (current.v != end.v)
-//                {
-//                    --length;
-//                    next = successors(current.x, current.y, grid, rows, cols);
+            ASObject end = new ASObject(e[0], e[1]);
+            end.V = e[0] + e[1] * cols;
 
-//                    for (i = 0, j = next.Length; i < j; ++i)
-//                    {
-//                        if (next[i] == null)
-//                        {
-//                            continue;
-//                        }
+            bool inList;
 
-//                        (adj = next[i]).p = current;
-//                        adj.f = adj.g = 0;
-//                        adj.v = adj.x + adj.y * cols;
-//                        inList = false;
+            do
+            {
+                max = limit;
+                min = 0;
 
-//                        foreach (int key in list)
-//                        {
-//                            if (adj.v == key)
-//                            {
-//                                inList = true;
-//                            }
-//                        }
+                for (i = 0; i < length; i++)
+                {
+                    if (open[i].F < max)
+                    {
+                        max = open[i].F;
+                        min = i;
+                    }
+                }
 
-//                        if (!inList)
-//                        {
-//                            if (this.find == "DiagonalFree" || this.find == "Diagonal")
-//                            {
-//                                distanceS = diagonal(adj, current);
-//                                distanceE = diagonal(adj, end);
-//                            }
-//                            else if (this.find == "Euclidean" || this.find == "EuclideanFree")
-//                            {
-//                                distanceS = euclidean(adj, current);
-//                                distanceE = euclidean(adj, end);
-//                            }
-//                            else
-//                            {
-//                                distanceS = manhattan(adj, current);
-//                                distanceE = manhattan(adj, end);
-//                            }
+                current = open[min];
+                open.RemoveAt(min);
 
-//                            adj.f = (adj.g = current.g + distanceS) + distanceE;
-//                            open.Add(adj);
-//                            list.Add(adj.v);
-//                            length++;
-//                        }
-//                    }
-//                }
-//                else
-//                {
-//                    i = length = 0;
-//                    do
-//                    {
-//                        this.result.Add(new Vector2(current.x, current.y));
-//                    }
-//                    while ((current = current.p) != null);
-//                    result.Reverse();
-//                }
-//            }
-//            while (length != 0);
-//        }
-//    }
-//}
+                if (current.V != end.V)
+                {
+                    --length;
+                    next = Successors(current.X, current.Y, grid, rows, cols);
+
+                    for (i = 0, j = next.Length; i < j; ++i)
+                    {
+                        if (next[i] == null)
+                        {
+                            continue;
+                        }
+
+                        (adj = next[i]).P = current;
+                        adj.F = adj.G = 0;
+                        adj.V = adj.X + adj.Y * cols;
+                        inList = false;
+
+                        foreach (int key in list)
+                        {
+                            if (adj.V == key)
+                            {
+                                inList = true;
+                            }
+                        }
+
+                        if (!inList)
+                        {
+                            if (this.find == "DiagonalFree" || this.find == "Diagonal")
+                            {
+                                distanceS = Diagonal(adj, current);
+                                distanceE = Diagonal(adj, end);
+                            }
+                            else if (this.find == "Euclidean" || this.find == "EuclideanFree")
+                            {
+                                distanceS = Euclidean(adj, current);
+                                distanceE = Euclidean(adj, end);
+                            }
+                            else
+                            {
+                                distanceS = Manhattan(adj, current);
+                                distanceE = Manhattan(adj, end);
+                            }
+
+                            adj.F = (adj.G = current.G + distanceS) + distanceE;
+                            open.Add(adj);
+                            list.Add(adj.V);
+                            length++;
+                        }
+                    }
+                }
+                else
+                {
+                    i = length = 0;
+                    do
+                    {
+                        this.result.Add(new Vector2(current.X, current.Y));
+                    }
+                    while ((current = current.P) != null);
+                    result.Reverse();
+                }
+            }
+            while (length != 0);
+        }
+    }
+}
